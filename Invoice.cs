de6bb9a01c19f6eab8b1cb04f34785ab4865b2aa
iradakhf <iradakhf@code.edu.az>
2022-07-11 20:56:43 +0400

    add 2nd method of 2nd task

---
+++ Invoice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Invoice
+    {
+       private string account;
+       private string customer;
+       private string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Invoice(double price)
+        {
+            this.Price = price;
+        }
+        public Invoice(string account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        public double CostCalculation ( bool needVat)
+        {
+           
+            
+            //if (Article == "LapTop")
+            //{
+            //    productValue = 1200;
+            //}
+            //else if (Article == "USB")
+            //{
+            //    productValue = 12;
+            //}
+            //else
+            //{
+            //    Article = "SD Card";
+            //    productValue = 30;
+            //}
+            double vat = this.Price* 0.18;
+            if (needVat == true)
+            {
+                this.Price = this.Price * Quantity + vat;
+            }
+            else
+            {
+                
+               this.Price *= Quantity;
+            }
+            return this.Price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Invoice invoice = new Invoice("1542", "Ird", "SuperMarket");
+            Invoice articleLapTop = new Invoice(1200);
+            articleLapTop.Article = "LapTop";
+            articleLapTop.Quantity = 2;
+            Invoice articleSdCard = new Invoice(30);
+            articleSdCard.Article = "SdCard";
+            articleSdCard.Quantity = 2;
+            Invoice articleUsb = new Invoice(12);
+            articleUsb.Article = "USB";
+            articleUsb.Quantity = 4;
+
+
+
+            Console.WriteLine($"{articleLapTop.Quantity} {articleLapTop.Article} : {articleLapTop.CostCalculation(true)} AZN ");
+        }
+
+    }
+}
